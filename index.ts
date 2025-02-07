682cae325f0e7c4ff7c2d33ec876060c51fdfd57
Arnav K <arnavkaushal09@gmail.com>
2025-02-07 14:22:59 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,12 +1,12 @@
 type Result<T, E = Error> = [T, null] | [null, E] 
   
- async function unwrapPromise<T>(promise: Promise<T>): Promise<Result<T>> { 
+export async function unwrapPromise<T>(promise: Promise<T>): Promise<Result<T>> { 
    return promise 
      .then<[T, null]>((data) => [data, null]) 
      .catch<[null, Error]>((err) => [null, err]) 
  } 
   
- function unwrapSyncError<T>(fn: () => T): Result<T> { 
+export function unwrapSync<T>(fn: () => T): Result<T> { 
    try { 
      return [fn(), null] 
    } catch (err) { 
